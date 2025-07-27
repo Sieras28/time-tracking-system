849fe74ab771b855049d35b20cb481c33a861869
Sieras28 <sergejgorbatenko@yahoo.com>
2025-07-27 12:43:09 +0000

    init: basic spring boot setup

---
+++ src/main/java/time/tracking/system/SpringBooTimeTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package time.tracking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooTimeTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooTimeTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/time/tracking/system/SpringBooTimeTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package time.tracking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooTimeTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
